53055bb0bde96c7a22705acb4d7d0cf9465eea86
tk14035 <petr.ljutik@teko.biz>
2018-07-31 16:08:33 +0300

    Заменил cin вводом данных из файла

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include "ver.h"
 #include "ip_filter.h"
+#include <fstream>
 
 
 
 int main(int argc, char const *argv[]) {
   std::cout << "Version: " << ver_major() << "." << ver_minor() << "." << ver_patch() << std::endl;
 
+  std::ifstream is ("ip_filter.tsv", std::ifstream::binary);
 
-  ip_pool_t ipPool = FormIpPool(std::cin);
+  ip_pool_t ipPool = FormIpPool(/*std::cin*/is);
 
   for(auto it: ipPool)
   {
     std::cout << "IP: ";
     for(auto it1: it)
-    {
-      std::cout << it1;
-    }
+      std::cout << it1 << ".";
     std::cout << std::endl;
   }
 
--- ver.cpp
+++ ver.cpp
@@ -1,17 +1,20 @@
 #include "ver.h"
-#include "version.h"
+//#include "version.h"
 
 int ver_major()
 {
-  return PROJECT_VERSION_MAJOR;
+//  return PROJECT_VERSION_MAJOR;
+  return 1;
 }
 
 int ver_minor()
 {
-  return PROJECT_VERSION_MINOR;
+//  return PROJECT_VERSION_MINOR;
+  return 1;
 }
 
 int ver_patch()
 {
-  return PROJECT_VERSION_PATCH;
+//  return PROJECT_VERSION_PATCH;
+  return 1;
 }
